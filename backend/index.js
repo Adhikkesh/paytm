1a5b9fcacb0a9c935e4004f3ffab9eb2487af557
ADHIKKESH S K <144268393+Adhikkesh@users.noreply.github.com>
2025-01-09 22:34:34 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,14 @@ import rootRouter from "./routes/index.js"
 import cors from "cors"
 import bodyParser from "body-parser";
 
+const corsOptions = {
+  origin: 'https://skar-pay-frontend.onrender.com',
+  methods: 'GET,POST,PUT,DELETE',
+  credentials: true,
+};
+
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
